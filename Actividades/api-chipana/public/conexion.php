86ca81d16a42ed6ccb4b302b0f3843d486a4b751
yasarevalo <143626048+yasarevalo@users.noreply.github.com>
2025-09-18 20:12:21 -0300

    Aplicación de las variables de entorno

--- Actividades/api-chipana/public/conexion.php
+++ Actividades/api-chipana/public/conexion.php
@@ -1,11 +1,17 @@
 <?php
-$host = 'localhost';  
-$db   = 'fliachipana';  
-$port = 3306;  
-$charset = 'utf8mb4';  
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$host     = $_ENV['DB_HOST'];
+$db       = $_ENV['DB_NAME'];
+$port     = $_ENV['DB_PORT'];
+$charset  = $_ENV['DB_CHARSET'];
+$usuario  = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
-$usuario = 'root';        
-$password = 'Yasminarevalo2005';  
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,    
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,         
@@ -14,9 +20,10 @@ $options = [
 
 try {
     $pdo = new PDO($dsn, $usuario, $password, $options);
- 
 } catch (PDOException $e) {
      error_log($e->getMessage());
     echo'Error al conectarse a la base de datos.';
 }
-?>
+
+
+?> 
